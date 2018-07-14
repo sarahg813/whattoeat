1a2343d19d75a8a075ddaa62851d32b79a448d24
Kit Jonathan Te <kjonathante@gmail.com>
2018-07-13 21:11:14 -0700

    add rapidapi lib and yelp api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,6 +31,9 @@ function initMap() {
   $(document).on('click', '#search', function() { 
     event.preventDefault();
     console.log( pos );
+
+    var food = $('#food').val().trim();
+    searchYelp( pos, food );
   });
 }
 
@@ -54,4 +57,46 @@ function getAddress(pos) {
     }
 
   })
+}
+
+function searchYelp(pos, food) {
+  var rapid = new RapidAPI("hungryteam_5b4654e7e4b004833ec2655e", "da70f8e1-fd7f-4da4-b6be-2add1e8c39d8");
+  
+  rapid.call('YelpAPI', 'getBusinesses', { 
+    'accessToken': 'sDKQbunPU1KnzeMLrV_TTqdY_RczqfSV1CiZC5XSqZsUr_dariJ7gaNSBnUjwmqkVEQmXGlmfvwSbYVoyXhVidzv_HzRO35udGwP1A3TBbGNWw1S0A758BtO89pGW3Yx',
+    'latitude': pos.lat,
+    'longitude': pos.lng,
+    'term': food,
+    'open_now': true,
+    'limit': 2
+
+  }).on('success', function (res) {
+    /*YOUR CODE GOES HERE*/ 
+    console.log(res);
+
+    if (res.total > 0) {
+      var val = res.businesses[0];
+
+      var imgURL = val.image_url;
+      var name = val.name;
+      var phone = val.display_phone;
+      var price = val.price;
+      var rating = val.rating;
+
+      var $img = $('<img>').attr({
+        alt: name,
+        src: imgURL,
+        width: '300px'
+      });
+      var $h2 = $('<h2>').text( name );
+
+      var $div = $('<div>');
+      $div.append( $img, $h2 );
+      $('body').append($div);
+
+    }
+  }).on('error', function (res) {
+    /*YOUR CODE GOES HERE*/ 
+    console.log(res);
+  });
 }
