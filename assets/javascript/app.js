0b1251203866d3890c634e037c7ecd245491de4e
Kit Jonathan Te <kjonathante@gmail.com>
2018-07-19 21:29:19 -0700

    change searchYelp parameter to use location instead of pos

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -285,7 +285,7 @@ function showPick(food, location, pos) {
   $(showInId).load('pick.html #pickPage', function(){
     $('#currAddress').text(location);
     $('#foodPick').text("You choose " + food);
-    searchYelp( pos, food );
+    searchYelp( food, location );
     searchEdemam( food );  
     $('#pickInfo').append( $('<button id="pick-change-btn">').text('Change') )
   });
@@ -415,13 +415,14 @@ function getAddress(pos) {
   })
 }
 
-function searchYelp(pos, food, count=1) {
+function searchYelp(food, location, count=1) {
   var rapid = new RapidAPI("hungryteam_5b4654e7e4b004833ec2655e", "da70f8e1-fd7f-4da4-b6be-2add1e8c39d8");
   
   rapid.call('YelpAPI', 'getBusinesses', { 
     'accessToken': 'sDKQbunPU1KnzeMLrV_TTqdY_RczqfSV1CiZC5XSqZsUr_dariJ7gaNSBnUjwmqkVEQmXGlmfvwSbYVoyXhVidzv_HzRO35udGwP1A3TBbGNWw1S0A758BtO89pGW3Yx',
-    'latitude': pos.lat,
-    'longitude': pos.lng,
+    // 'latitude': pos.lat,
+    // 'longitude': pos.lng,
+    'location': location,
     'term': food,
     'open_now': true,
     'limit': count
